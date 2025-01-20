7bafbccfbee7f8753cff5e97b24a198cf74f573e
ali aslani <aliaslani.mm@gmail.com>
2025-01-20 19:05:05 +0330

    fix: add elastic logger

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 from app.api.V1.auth import AuthRouter
 from app.api.V1.user import UserRouter
+from app.modules.common.elastic import configure_logger
+
+# Configure logger
+logger = configure_logger()
+
+
 app = FastAPI()
 
 app.include_router(UserRouter.router, prefix="/api/v1/users", tags=["Users"])
---
+++ app/modules/common/elastic/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import socket
+from pythonjsonlogger import jsonlogger
+from elasticsearch import Elasticsearch
+
+# Get Elasticsearch details from environment variables
+import os
+ELASTICSEARCH_HOST = os.getenv("ELASTICSEARCH_HOST", "http://localhost:9200")
+ELASTICSEARCH_INDEX = os.getenv("ELASTICSEARCH_INDEX", "fastapi-logs")
+
+# Configure Elasticsearch client
+es_client = Elasticsearch(hosts=[ELASTICSEARCH_HOST])
+
+# Custom Elasticsearch handler
+class ElasticSearchHandler(logging.Handler):
+    def __init__(self, es_client, index_name):
+        super().__init__()
+        self.es_client = es_client
+        self.index_name = index_name
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.es_client.index(index=self.index_name, body=log_entry)
+
+# Configure logger
+def configure_logger():
+    logger = logging.getLogger("fastapi")
+    logger.setLevel(logging.INFO)
+
+    # Elasticsearch handler
+    es_handler = ElasticSearchHandler(es_client, ELASTICSEARCH_INDEX)
+    es_handler.setLevel(logging.INFO)
+
+    # JSON formatter
+    formatter = jsonlogger.JsonFormatter(
+        '%(asctime)s %(levelname)s %(name)s %(message)s'
+    )
+    es_handler.setFormatter(formatter)
+
+    logger.addHandler(es_handler)
+    return logger
